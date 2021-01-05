017fb28cb457611855f97ded9d6ff88842212245
YUKICC <dayouzicc@gmail.com>
2021-01-05 14:32:10 +0800

    更新

---
+++ C#代码/TEST_MDI_DEMO/TEST_MDI_DEMO/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEST_MDI_DEMO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true; // 将当前窗体设为父窗体
+            Form2 ParentObj1 = new Form2();//创建窗体对象
+            ParentObj1.MdiParent = this; // 将窗体设置为当前父窗体的子窗体
+            ParentObj1.Show();//显示窗体
+        }
+    }
+}
---
+++ C#代码/testTgreadDemo/testTgreadDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testTgreadDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("主线程开始");
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start(); //开启辅助线程
+            Thread.Sleep(3000);//休眠3秒
+            t.Resume(); //恢复被挂起线程
+            for(char i = 'a'; i < 'i'; i++)
+            {
+                Console.WriteLine("主线程挂起");
+                Thread.Sleep(1000);
+            }
+            t.Join();//辅助线程加入
+            Console.WriteLine("主线程结束");
+            Console.ReadLine();
+        }
+        static void ThreadMethod()
+        {
+            Console.WriteLine("辅助线程1");
+            Thread.CurrentThread.Suspend(); // 挂起当前线程
+            Console.WriteLine("辅助线程开启");
+            for(int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("辅助线程" + i);
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine("辅助线程结束");
+        }
+    }
+}
