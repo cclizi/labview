1249e35fbf03a289df9f45b4ee15e630bcfe5292
YUKICC <dayouzicc@gmail.com>
2021-01-07 14:38:28 +0800

    更新

---
+++ C#代码/HZTTESTDEMO/HZTTESTDEMO/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace HZTTESTDEMO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            HslControls.Charts.Axis axis3 = new HslControls.Charts.Axis();
+            HslControls.Charts.AxisLabel axisLabel13 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel14 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel15 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel16 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel17 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel18 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.Axis axis4 = new HslControls.Charts.Axis();
+            HslControls.Charts.AxisLabel axisLabel19 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel20 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel21 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel22 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel23 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.AxisLabel axisLabel24 = new HslControls.Charts.AxisLabel();
+            HslControls.Charts.Series.BezierSeries bezierSeries5 = new HslControls.Charts.Series.BezierSeries();
+            HslControls.Charts.Series.BezierSeries bezierSeries6 = new HslControls.Charts.Series.BezierSeries();
+            HslControls.Charts.Series.BezierSeries bezierSeries7 = new HslControls.Charts.Series.BezierSeries();
+            HslControls.Charts.Series.BezierSeries bezierSeries8 = new HslControls.Charts.Series.BezierSeries();
+            this.btn_OK = new HZH_Controls.Controls.UCBtnExt();
+            this.Btn_Exti = new HZH_Controls.Controls.UCBtnExt();
+            this.boxingtu = new HZH_Controls.Controls.UCWaveChart();
+            this.boxingtu1 = new HslControls.HslChart();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.BackColor = System.Drawing.Color.White;
+            this.btn_OK.BtnBackColor = System.Drawing.Color.White;
+            this.btn_OK.BtnFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_OK.BtnForeColor = System.Drawing.Color.White;
+            this.btn_OK.BtnText = "确定";
+            this.btn_OK.ConerRadius = 5;
+            this.btn_OK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_OK.EnabledMouseEffect = false;
+            this.btn_OK.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_OK.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.btn_OK.IsRadius = true;
+            this.btn_OK.IsShowRect = true;
+            this.btn_OK.IsShowTips = false;
+            this.btn_OK.Location = new System.Drawing.Point(741, 48);
+            this.btn_OK.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_OK.RectWidth = 1;
+            this.btn_OK.Size = new System.Drawing.Size(142, 46);
+            this.btn_OK.TabIndex = 0;
+            this.btn_OK.TabStop = false;
+            this.btn_OK.TipsColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_OK.TipsText = "";
+            this.btn_OK.BtnClick += new System.EventHandler(this.btn_OK_BtnClick);
+            // 
+            // Btn_Exti
+            // 
+            this.Btn_Exti.BackColor = System.Drawing.Color.White;
+            this.Btn_Exti.BtnBackColor = System.Drawing.Color.White;
+            this.Btn_Exti.BtnFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_Exti.BtnForeColor = System.Drawing.Color.White;
+            this.Btn_Exti.BtnText = "退出";
+            this.Btn_Exti.ConerRadius = 5;
+            this.Btn_Exti.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Exti.EnabledMouseEffect = false;
+            this.Btn_Exti.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Btn_Exti.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.Btn_Exti.IsRadius = true;
+            this.Btn_Exti.IsShowRect = true;
+            this.Btn_Exti.IsShowTips = false;
+            this.Btn_Exti.Location = new System.Drawing.Point(999, 48);
+            this.Btn_Exti.Margin = new System.Windows.Forms.Padding(0);
+            this.Btn_Exti.Name = "Btn_Exti";
+            this.Btn_Exti.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Btn_Exti.RectWidth = 1;
+            this.Btn_Exti.Size = new System.Drawing.Size(142, 46);
+            this.Btn_Exti.TabIndex = 1;
+            this.Btn_Exti.TabStop = false;
+            this.Btn_Exti.TipsColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Btn_Exti.TipsText = "";
+            this.Btn_Exti.BtnClick += new System.EventHandler(this.Btn_Exti_BtnClick);
+            // 
+            // boxingtu
+            // 
+            this.boxingtu.ConerRadius = 10;
+            this.boxingtu.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.boxingtu.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.boxingtu.GridLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.boxingtu.GridLineTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.boxingtu.IsRadius = true;
+            this.boxingtu.IsShowRect = true;
+            this.boxingtu.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.boxingtu.LineTension = 0.5F;
+            this.boxingtu.Location = new System.Drawing.Point(47, 14);
+            this.boxingtu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.boxingtu.Name = "boxingtu";
+            this.boxingtu.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.boxingtu.RectWidth = 1;
+            this.boxingtu.Size = new System.Drawing.Size(674, 283);
+            this.boxingtu.SleepTime = 1000;
+            this.boxingtu.TabIndex = 2;
+            this.boxingtu.WaveWidth = 50;
+            // 
+            // boxingtu1
+            // 
+            axis3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            axisLabel13.Content = "0";
+            axisLabel13.Value = 0D;
+            axisLabel14.Content = "0.2";
+            axisLabel14.Value = 0.2D;
+            axisLabel15.Content = "0.4";
+            axisLabel15.Value = 0.4D;
+            axisLabel16.Content = "0.6";
+            axisLabel16.Value = 0.60000000000000009D;
+            axisLabel17.Content = "0.8";
+            axisLabel17.Value = 0.8D;
+            axisLabel18.Content = "1";
+            axisLabel18.Value = 1D;
+            axis3.Labels.Add(axisLabel13);
+            axis3.Labels.Add(axisLabel14);
+            axis3.Labels.Add(axisLabel15);
+            axis3.Labels.Add(axisLabel16);
+            axis3.Labels.Add(axisLabel17);
+            axis3.Labels.Add(axisLabel18);
+            this.boxingtu1.AxisX.Add(axis3);
+            this.boxingtu1.AxisXIndexForSelect = 0;
+            axis4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            axisLabel19.Content = "0";
+            axisLabel19.Value = 0D;
+            axisLabel20.Content = "0.2";
+            axisLabel20.Value = 0.2D;
+            axisLabel21.Content = "0.4";
+            axisLabel21.Value = 0.4D;
+            axisLabel22.Content = "0.6";
+            axisLabel22.Value = 0.60000000000000009D;
+            axisLabel23.Content = "0.8";
+            axisLabel23.Value = 0.8D;
+            axisLabel24.Content = "1";
+            axisLabel24.Value = 1D;
+            axis4.Labels.Add(axisLabel19);
+            axis4.Labels.Add(axisLabel20);
+            axis4.Labels.Add(axisLabel21);
+            axis4.Labels.Add(axisLabel22);
+            axis4.Labels.Add(axisLabel23);
+            axis4.Labels.Add(axisLabel24);
+            this.boxingtu1.AxisY.Add(axis4);
+            this.boxingtu1.AxisYIndexForSelect = 0;
+            this.boxingtu1.BackColor = System.Drawing.Color.White;
+            this.boxingtu1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.boxingtu1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.boxingtu1.HorizontalGridVisible = false;
+            this.boxingtu1.HorizontalSeparatorSum = 5;
+            this.boxingtu1.LegendFont = new System.Drawing.Font("微软雅黑", 12F);
+            this.boxingtu1.Location = new System.Drawing.Point(47, 305);
+            this.boxingtu1.Name = "boxingtu1";
+            this.boxingtu1.OffsetX = 0;
+            this.boxingtu1.OffsetY = 0;
+            this.boxingtu1.Padding = new System.Windows.Forms.Padding(6);
+            bezierSeries5.Color = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(127)))), ((int)(((byte)(184)))));
+            bezierSeries6.Color = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            bezierSeries7.Color = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(195)))), ((int)(((byte)(15)))));
+            bezierSeries8.Color = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(187)))), ((int)(((byte)(155)))));
+            this.boxingtu1.Series.Add(bezierSeries5);
+            this.boxingtu1.Series.Add(bezierSeries6);
+            this.boxingtu1.Series.Add(bezierSeries7);
+            this.boxingtu1.Series.Add(bezierSeries8);
+            this.boxingtu1.Size = new System.Drawing.Size(674, 270);
+            this.boxingtu1.TabIndex = 3;
+            this.boxingtu1.TabStop = false;
+            this.boxingtu1.Text = "hslChart1";
+            this.boxingtu1.VerticalGridVisible = false;
+            this.boxingtu1.VerticalSeparatorSum = 5;
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(728, 208);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(431, 208);
+            this.cartesianChart1.TabIndex = 4;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 611);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.boxingtu1);
+            this.Controls.Add(this.boxingtu);
+            this.Controls.Add(this.Btn_Exti);
+            this.Controls.Add(this.btn_OK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HZH_Controls.Controls.UCBtnExt btn_OK;
+        private HZH_Controls.Controls.UCBtnExt Btn_Exti;
+        private HZH_Controls.Controls.UCWaveChart boxingtu;
+        private HslControls.HslChart boxingtu1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+    }
+}
+
